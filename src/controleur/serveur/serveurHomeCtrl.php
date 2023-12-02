96a6983c5b23acde1dfc9ee1f072e532bd323c0d
Motard80 <gaetan.wilbert@outlook.fr>
2023-12-02 12:49:21 +0100

    Essaie de correction beug message serveur

--- src/controleur/serveur/serveurHomeCtrl.php
+++ src/controleur/serveur/serveurHomeCtrl.php
@@ -19,6 +19,7 @@ if (file_exists($cheminFichierJSON)) {
     $jsonContent = file_get_contents($cheminFichierJSON);
     $profil = json_decode($jsonContent, true);
 }
+var_dump($profil);
 $title='gestion du serveur';
 if(isset($_POST['start'])){
     if(!empty($_POST['profil'])){
@@ -28,19 +29,15 @@ if(isset($_POST['start'])){
         $error['start']='Sélectionner un profils';
     }
 
-}
-if(isset($_POST['reStart'])){
+}elseif(isset($_POST['reStart'])){
     if(!empty($_POST['profil'])){
         Restart::restart();
-        $message['reStart']='Serveur redémarer';
+        $message['reStart']='Serveur redémarer'; 
     }else{
         $error['reStart']='selectionner un profil';
     }
-}
-
-if(isset($_POST['stop'])){
+}elseif(isset($_POST['stop'])){
     Stop::stopServeur();
-    
     $message['stop']='Serveur arreter';
     
 }else{
--- src/view/serveur/serveurHome.php
+++ src/view/serveur/serveurHome.php
@@ -6,8 +6,6 @@ use src\class\otherClass\Form;
 $powerShellExecutor = new PowerShellExecutor();
 include_once '../src/controleur/serveur/serveurHomeCtrl.php';
 $form = new Form();
-$error;
-$message;
 ?>
 <title><?= isset($title) ? $title : '' ?></title>
 </head>
@@ -22,6 +20,10 @@ $message;
                 <h1>Gestion du serveur</h1>
             </div>
         </div>
+        <?php
+        $output = shell_exec('cat /etc/hosts');
+        echo "<pre>$output</pre>";
+        ?>
     </div>
     <div>
         <form action="" method="post" name="serveur">
@@ -32,39 +34,39 @@ $message;
                         <?php
                         // Générez les options à partir du tableau JSON
                         foreach ($profil as $profils) { ?>
-                            <option value="<?= $profils ?>"><?= $profils ?></option>';
-
-                        <?php  }
+                            <option value="<?= isset($profils) ? $profils : '' ?>"><?= isset($profils) ? $profils : '' ?></option>
+                        <?php 
+                         }
                         ?>
                     </select>
                 </div>
                 <div class="row justify-content-start">
                     <div class="col-4">
                         <?= $form->submit('démarer le serveur', 'start', 'start') ?>
-        <div id="messageServeurOk" class="row justify-content-center">
-            <?= isset($message['start'])? $message['start'] :''?>
-        </div>
-        <div id="messageServeurNoOk" class="row justify-content-center">
-            <?=isset($error['start'])? $error['start'] :'' ?>
-        </div>
+                        <div id="messageServeurOk" class="row justify-content-center">
+                            <?= isset($message['start']) ? $message['start'] : '' ?>
+                        </div>
+                        <div id="messageServeurNoOk" class="row justify-content-center">
+                            <?= isset($error['start']) ? $error['start'] : '' ?>
+                        </div>
                     </div>
                     <div class="col-4">
                         <?= $form->submit('Restart le serveur', 'reStart', 'reStart') ?>
-        <div id="messageServeurOk" class="row justify-content-center">
-            <?= isset($message['reStart'])? $message['reStart'] :''?>
-        </div>
-        <div id="messageServeurNoOk" class="row justify-content-center"> 
-            <?=isset($error['reStart'])? $error['reStart'] :'' ?>
-        </div>
+                        <div id="messageServeurOk" class="row justify-content-center">
+                            <?= isset($message['reStart']) ? $message['reStart'] : '' ?>
+                        </div>
+                        <div id="messageServeurNoOk" class="row justify-content-center">
+                            <?= isset($error['reStart']) ? $error['reStart'] : '' ?>
+                        </div>
                     </div>
                     <div class="col-4">
                         <?= $form->submit('Stoper le serveur', 'stop', 'stop') ?>
-        <div id="messageServeurOk" class="row justify-content-center">
-            <?= isset($message['stop'])? $message['stop'] :''?>
-        </div>
-        <div id="messageServeurNoOk" class="row justify-content-center">
-            <?= isset($error['stop'])? $error['stop'] :'' ?>
-        </div>
+                        <div id="messageServeurOk" class="row justify-content-center">
+                            <?= isset($message['stop']) ? $message['stop'] : '' ?>
+                        </div>
+                        <div id="messageServeurNoOk" class="row justify-content-center">
+                            <?= isset($error['stop']) ? $error['stop'] : '' ?>
+                        </div>
                     </div>
                 </div>
             </div>
