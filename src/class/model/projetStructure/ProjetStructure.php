e53f78a5e245a188a230ea3b7d02dacac64d8051
Motard80 <gaetan.wilbert@outlook.fr>
2023-12-04 10:52:54 +0100

    Modification du script ProjectStructure pour qu'un fichier json soit créer

--- src/class/model/projetStructure/ProjetStructure.php
+++ src/class/model/projetStructure/ProjetStructure.php
@@ -10,7 +10,6 @@ class ProjetStructure
     {
         $this->cheminParent = $cheminParent;
     }
-
     public function creerStructure($profils)
     {
         $nomProjet = $profils;
@@ -24,9 +23,7 @@ class ProjetStructure
             $errorCreate = 'La création du dossier a échoué! Le dossier existe déjà.';
             return $errorCreate;
         }
-
         $source = '../src/view/profil_1/config.cfg';
-
         // Vérifier si le fichier source existe avant de le copier
         if (file_exists($source)) {
             if (copy($source, $cheminProjet . '/config.cfg')) {
@@ -37,14 +34,13 @@ class ProjetStructure
         } else {
             $errorCreate = 'Le fichier source n\'existe pas.';
         }
-
         // Créer les sous-dossiers dans le dossier principal
         $this->creerSousDossier($cheminProjet, 'mission');
         $this->creerSousDossier($cheminProjet, 'presset');
-
+        // Inclure et renommer le fichier profil.json
+        $this->inclureEtRenommerProfil($cheminProjet, $nomProjet);
         return $messageCreate ?? $errorCreate;
     }
-
     private function creerSousDossier($dossierParent, $nomSousDossier)
     {
         $chemin = $dossierParent . '/' . $nomSousDossier;
@@ -59,24 +55,32 @@ class ProjetStructure
         if (!file_exists($nouveauRepertoire)) {
             mkdir($nouveauRepertoire, 0777, true);
         }
-    
+
         // Charger le contenu actuel du fichier JSON
         $source = $nouveauRepertoire . '/config.cfg';
         $configContent = file_get_contents($source);
         $configArray = json_decode($configContent, true);
-    
+
         // Modifier la classe Missions pour le profil donné
         $configArray['profil'] = $profil;
-    
+
         // Enregistrer le tableau mis à jour dans un fichier JSON dans le nouveau répertoire
         $nouveauChemin = $nouveauRepertoire . '/' . $profil . '.json';
         file_put_contents($nouveauChemin, json_encode($configArray));
-    
         // Supprimer l'ancien fichier de configuration s'il existe
         if (file_exists($source)) {
             unlink($source);
         }
+    }private function inclureEtRenommerProfil($dossierProjet, $nomProjet)
+    {
+        $cheminFichierProfilSource = '../src/view/profil_1/profil.json';
+        $cheminFichierProfilDestination = $dossierProjet . '/' . $nomProjet . '.json';
+    
+        // Copier le fichier profil.json dans le nouveau projet
+        if (copy($cheminFichierProfilSource, $cheminFichierProfilDestination)) {
+            $succes = 'Le fichier profil.json a été ajouté avec succès.';
+        } else {
+            $error = 'L\'ajout du fichier profil.json a échoué.';
+        }
     }
-    
-    
 }
